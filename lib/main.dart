4c7133e501280372e84b5c36712bd8f647f95a8d
Abhay Chaudhary <chaudharyabhay111@gmail.com>
2021-08-04 01:45:53 +0530

    day 2

--- lib/main.dart
+++ lib/main.dart
@@ -1,49 +1,74 @@
 import 'package:flutter/material.dart';
+import './questions.dart';
+import './answer.dart';
 
 void main() {
   runApp(MyApp());
 }
 
 //extends inherit from another class here its inbuilt flutter
-class MyApp extends StatelessWidget {
-  var questionindex = 0;
-  void answers() {
-    questionindex = questionindex + 1;
-    print(questionindex);
+class MyApp extends StatefulWidget {
+  @override
+  _MyAppState createState() => _MyAppState();
+}
+
+class _MyAppState extends State<MyApp> {
+  final questions = const [
+    {
+      'questiontext': 'what\'s your fav color?',
+      'answers': ['Black', 'Red', 'White', 'Green'],
+    },
+    {
+      'questiontext': 'what\'s your fav animal?',
+      'answers': ['RAt', 'Rabbit', 'Lion', 'Tiger'],
+    },
+    {
+      'questiontext': 'what\'s your fav ice-cream',
+      'answers': ['Tootifooti', 'Mango', 'Chocolate', 'Vanilla'],
+    },
+  ];
+  var _questionindex = 0;
+  @override
+  void _answers() {
+    //setstate is a fn used to rerender the interface or it call build again
+
+    setState(() {
+      //update
+      _questionindex = _questionindex + 1;
+    });
+    print(_questionindex);
+    if (_questionindex < questions.length) {
+      print('We have more questions..');
+    }
   }
 
   @override
   Widget build(BuildContext context) {
-    var questions = [
-      'what\'s your fav color',
-      'what\'s your fav animal?',
-    ];
     return MaterialApp(
       home: Scaffold(
-          appBar: AppBar(
-            title: Text('Myappp bar'),
-          ),
-          body: Column(
-            children: [
-              Text(
-                questions[questionindex],
+        appBar: AppBar(
+          title: Text('Myappp bar'),
+        ),
+        body: _questionindex < questions.length
+            ? Column(
+                children: [
+                  Questions(
+                    //as sting addded due to error
+                    questions[_questionindex]['questiontext'] as String,
+                  ),
+                  //accessing the widgets without hardcoding it
+                  //here accessing list of widgets by mapping
+                  ...(questions[_questionindex]['answers'] as List<String>)
+                      //acessing the questions with key['answers'] with list of stings
+                      .map((answer) {
+                    return Answers(_answers, answer);
+                  }).toList() //convert value of map to list
+                ],
+              )
+            : Center(
+                child: Text('U did it'),
               ),
-              RaisedButton(
-                child: Text('Answer 1'),
-                onPressed: answers,
-              ),
-              RaisedButton(
-                child: Text('Answer 2'),
-                onPressed: () => print('answer 2 choosen'),
-              ),
-              RaisedButton(
-                child: Text('Answer 3'),
-                onPressed: () {
-                  print('Answer 3 choosen');
-                },
-              ),
-            ],
-          )),
+      ),
     );
   }
 }
